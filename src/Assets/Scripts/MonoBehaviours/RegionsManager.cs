f2e01bfbe8dfbb86481db6bd6854c3c153b06743
fre-ber <fre-ber@hotmail.com>
2021-01-02 17:48:59 +0100

    Added Terrain and placeholders for water and the player.

---
+++ src/Assets/Scripts/MonoBehaviours/RegionsManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class RegionsManager : MonoBehaviour
+{
+    [SerializeField] protected GameObject CurrentRegion;
+
+    protected class RegionGo
+    {
+        public Terrain Terrain;
+        public Water Water;
+    }
+
+    protected RegionGo CurrentRegionGo;
+
+    private void Start()
+    {
+        CurrentRegionGo = new RegionGo()
+        {
+            Terrain = GetComponentInChildren<Terrain>(),
+            Water = GetComponentInChildren<Water>()
+        };
+
+        EventManager.Instance.OnRegionDataChanged += OnRegionDataChanged;
+    }
+
+    protected void OnRegionDataChanged(Region region)
+    {
+        if (region == Region.CurrentRegion)
+        {
+            Vector3 pos = CurrentRegionGo.Water.transform.position;
+            pos.y = region.WaterHeight;
+            CurrentRegionGo.Water.transform.position = pos;
+        }
+    }
+}
