3773436de47ee65af2ff019522134296bb9e7ca4
ApanLoon <apanloon@live.co.uk>
2021-01-07 20:50:25 +0100

    Added mini map. - Markers with tool tips and colour - No map layers

---
+++ src/Assets/Scripts/MonoBehaviours/UI/Floater/MiniMap.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniMap : MonoBehaviour
+{
+    [SerializeField] protected GameObject MarkerPrefab;
+    [SerializeField] protected RectTransform MarkerContainer;
+
+    protected class MarkerInfo
+    {
+        public GameObject GameObject;
+        public RectTransform Transform;
+        public Image Image;
+        public TMP_Text Text;
+    }
+
+    protected Dictionary<Guid, MarkerInfo> MarkerInfoByAgentId = new Dictionary<Guid, MarkerInfo>();
+
+    private void Start()
+    {
+        EventManager.Instance.OnCoarseLocationUpdateMessage += OnCoarseLocationUpdateMessage;
+    }
+
+    protected void OnCoarseLocationUpdateMessage(CoarseLocationUpdateMessage message)
+    {
+        Transform youTransform = null;
+        Transform preyTransform = null;
+        foreach (CoarseLocation location in message.Locations)
+        {
+            MarkerInfo mi;
+            if (MarkerInfoByAgentId.ContainsKey(location.AgentId))
+            {
+                mi = MarkerInfoByAgentId[location.AgentId];
+            }
+            else
+            {
+                mi = new MarkerInfo();
+                mi.GameObject = Instantiate(MarkerPrefab, MarkerContainer);
+                mi.Transform = mi.GameObject.GetComponent<RectTransform>();
+                mi.Image = mi.GameObject.GetComponent<Image>();
+                mi.Text = mi.GameObject.GetComponentInChildren<TMP_Text>(true);
+
+                MarkerInfoByAgentId[location.AgentId] = mi;
+            }
+            
+            mi.Transform.anchoredPosition = ScalePosition(location.Position);
+
+            Color c = Color.green;
+            if (location.IsYou)
+            {
+                c = Color.white;
+                youTransform = mi.Transform;
+            }
+            if (location.IsPrey)
+            {
+                c = Color.red;
+                preyTransform = mi.Transform;
+            }
+            mi.Image.color = c;
+
+            mi.Text.text = location.AgentId.ToString(); // TODO: Look up name
+            
+        }
+
+        // Force visibility of prey and you:
+        if (preyTransform != null)
+        {
+            preyTransform.SetAsLastSibling();
+        }
+
+        if (youTransform != null)
+        {
+            youTransform.SetAsLastSibling();
+        }
+
+        // Remove markers that have left:
+        foreach (Guid id in MarkerInfoByAgentId.Keys.Where(id => message.Locations.FirstOrDefault((x) => x.AgentId == id) == null))
+        {
+            Destroy(MarkerInfoByAgentId[id].GameObject);
+            MarkerInfoByAgentId.Remove(id);
+        }
+    }
+
+    protected Vector2 ScalePosition(Vector3Byte position)
+    {
+        return new Vector2(MarkerContainer.rect.width * position.x / 255f, MarkerContainer.rect.height * position.z / 255f);
+    }
+}
