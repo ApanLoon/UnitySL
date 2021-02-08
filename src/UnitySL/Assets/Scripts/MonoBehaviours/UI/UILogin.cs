c1bf0dff00cc952bb1f3f7aa8456794fb5ec50b9
Thor Brigsted <thorbrigsted@gmail.com>
2021-02-08 10:03:55 +0100

    Disable UI interactivity while logging in

--- src/UnitySL/Assets/Scripts/MonoBehaviours/UI/UILogin.cs
+++ src/UnitySL/Assets/Scripts/MonoBehaviours/UI/UILogin.cs
@@ -7,74 +7,102 @@ using System;
 
 public class UILogin : MonoBehaviour
 {
-	public TMP_InputField nameInput;
-	public TMP_InputField passwordInput;
-	public Toggle saveNameToggle;
-	public Toggle savePasswordToggle;
-	public UICredentialTemplate savedCredentials;
-	public Button loginButton;
+    public CanvasGroup group { get { return _group != null ? _group : _group = GetComponent<CanvasGroup>(); } }
+    private CanvasGroup _group;
 
-	private void Start()
-	{
-		savedCredentials.Initialize();
-		saveNameToggle.isOn = Settings.instance.login.saveName;
-		savePasswordToggle.isOn = Settings.instance.login.savePassword;
-		ReloadCredentials();
-		ValidateInput();
-	}
+    public TMP_InputField nameInput;
+    public TMP_InputField passwordInput;
+    public Toggle saveNameToggle;
+    public Toggle savePasswordToggle;
+    public UICredentialTemplate savedCredentials;
+    public Button loginButton;
 
-	public void ValidateInput()
-	{
-		loginButton.interactable = !string.IsNullOrEmpty(nameInput.text) && !string.IsNullOrEmpty(passwordInput.text);
-	}
+    private void Start()
+    {
+        savedCredentials.Initialize();
+        saveNameToggle.isOn = Settings.instance.login.saveName;
+        savePasswordToggle.isOn = Settings.instance.login.savePassword;
+        Open();
+    }
 
-	public async void Login()
-	{
-		Credential credential = new Credential(nameInput.text, passwordInput.text);
-		await Session.Instance.Start(credential);
-		if (saveNameToggle)
-		{
-			CredentialStorage.Instance.Store(nameInput.text, savePasswordToggle.isOn ? passwordInput.text : "");
-		}
-	}
+    public void Open()
+    {
+        group.interactable = true;
+        gameObject.SetActive(true);
+        nameInput.text = "";
+        passwordInput.text = "";
+        ReloadCredentials();
+        ValidateInput();
+    }
 
-	public void SaveCredentialPreferences()
-	{
-		Settings.instance.login.saveName = saveNameToggle.isOn;
-		if (!saveNameToggle.isOn)
-		{
-			savePasswordToggle.isOn = false;
-		}
-		savePasswordToggle.interactable = saveNameToggle.isOn;
-		Settings.instance.login.saveName = saveNameToggle.isOn;
-		Settings.instance.login.savePassword = savePasswordToggle.isOn;
-		Settings.instance.Save();
-	}
+    public void Close()
+    {
+        group.interactable = false;
+        gameObject.SetActive(false);
+    }
 
-	private void ReloadCredentials()
-	{
-		savedCredentials.Clear();
-		foreach (Credential c in CredentialStorage.Instance)
-		{
-			UICredential uic = savedCredentials.InstantiateTemplate();
-			uic.label.text = $"{c.First} {c.Last}";
-			uic.button.onClick.AddListener(() => { LoadCredential(c); });
-			uic.buttonX.onClick.AddListener(() => { DeleteCredential(c); });
-		}
-	}
+    public void ValidateInput()
+    {
+        loginButton.interactable = !string.IsNullOrEmpty(nameInput.text) && !string.IsNullOrEmpty(passwordInput.text);
+    }
 
-	protected void LoadCredential(Credential credential)
-	{
-		nameInput.text = $"{credential.First} {credential.Last}";
-		passwordInput.text = credential.Secret;
-	}
+    public async void Login()
+    {
+        group.interactable = false;
+        Credential credential = new Credential(nameInput.text, passwordInput.text);
+        await Session.Instance.Start(credential);
+        if (Session.Instance.IsLoggedIn)
+        {
+            Close();
+            if (saveNameToggle)
+            {
+                CredentialStorage.Instance.Store(nameInput.text, savePasswordToggle.isOn ? passwordInput.text : "");
+            }
+        }
+        else
+        {
+            // Error logging in, enable UI again
+            group.interactable = true;
+        }
+    }
 
-	protected void DeleteCredential(Credential credential)
-	{
-		CredentialStorage.Instance.Remove(credential);
-		ReloadCredentials();
-	}
+    public void SaveCredentialPreferences()
+    {
+        Settings.instance.login.saveName = saveNameToggle.isOn;
+        if (!saveNameToggle.isOn)
+        {
+            savePasswordToggle.isOn = false;
+        }
+        savePasswordToggle.interactable = saveNameToggle.isOn;
+        Settings.instance.login.saveName = saveNameToggle.isOn;
+        Settings.instance.login.savePassword = savePasswordToggle.isOn;
+        Settings.instance.Save();
+    }
+
+    private void ReloadCredentials()
+    {
+        savedCredentials.Clear();
+        foreach (Credential c in CredentialStorage.Instance)
+        {
+            UICredential uic = savedCredentials.InstantiateTemplate();
+            uic.label.text = $"{c.First} {c.Last}";
+            uic.button.onClick.AddListener(() => { LoadCredential(c); });
+            uic.buttonX.onClick.AddListener(() => { DeleteCredential(c); });
+        }
+    }
+
+    protected void LoadCredential(Credential credential)
+    {
+        nameInput.text = $"{credential.First} {credential.Last}";
+        passwordInput.text = credential.Secret;
+    }
+
+    protected void DeleteCredential(Credential credential)
+    {
+        CredentialStorage.Instance.Remove(credential);
+        ReloadCredentials();
+    }
 
 
-	[Serializable] public class UICredentialTemplate : Template<UICredential> { }
+    [Serializable] public class UICredentialTemplate : Template<UICredential> { }
 }
