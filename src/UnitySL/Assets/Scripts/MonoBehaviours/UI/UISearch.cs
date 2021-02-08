0b6bed3aad3410f502ea9b7e8e569c4fccb8d1c8
Thor Brigsted <thorbrigsted@gmail.com>
2021-02-08 16:24:48 +0100

    Attempt to parse the returned XML format

--- src/UnitySL/Assets/Scripts/MonoBehaviours/UI/UISearch.cs
+++ src/UnitySL/Assets/Scripts/MonoBehaviours/UI/UISearch.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Xml.Serialization;
+using System.IO;
+using System.Text;
 
 public enum MaturityRating { PG = 1, Mature = 2, Adult = 4 }
 
@@ -23,7 +26,7 @@ public class UISearch : MonoBehaviour
         if (mature) rating |= MaturityRating.Mature;
         if (adult) rating |= MaturityRating.Adult;
         WWWFormPlus form = new WWWFormPlus();
-        string url = $"http://search.secondlife.com/client_search.php?q=hippo&start={start}&mat={(int)rating}&output=xml_no_dtd&s={category.ToString()}";
+        string url = $"http://search.secondlife.com/client_search.php?q=hippo&start={start}&mat={(int)rating}&output=xml_no_dtd&client = raw_xml_frontend&s={category.ToString()}";
         Debug.Log(url);
         form.Request(url, OnFail, OnSuccess);
     }
@@ -35,7 +38,13 @@ public class UISearch : MonoBehaviour
 
     public void OnSuccess(string text)
     {
-        Debug.Log("Success: " + text);
+        XmlSerializer serializer = new XmlSerializer(typeof(SearchResultXML.Html));
+        // convert string to stream
+        byte[] byteArray = Encoding.UTF8.GetBytes(text);
+        //byte[] byteArray = Encoding.ASCII.GetBytes(contents);
+        MemoryStream stream = new MemoryStream(byteArray);
+        SearchResultXML.Html results = serializer.Deserialize(stream) as SearchResultXML.Html;
+        Debug.Log(results.Body.Wrapper.Divs.Count);
 
     }
 }
---
+++ src/UnitySL/Assets/Scripts/XML/SearchResultXML.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Xml.Serialization;
+
+public class SearchResultXML
+{
+    [XmlRoot(ElementName = "meta", Namespace = "http://www.w3.org/1999/xhtml")]
+    public class Meta
+    {
+        [XmlAttribute(AttributeName = "http-equiv")]
+        public string Httpequiv { get; set; }
+        [XmlAttribute(AttributeName = "content")]
+        public string Content { get; set; }
+    }
+
+    [XmlRoot(ElementName = "link", Namespace = "http://www.w3.org/1999/xhtml")]
+    public class Link
+    {
+        [XmlAttribute(AttributeName = "href")]
+        public string Href { get; set; }
+        [XmlAttribute(AttributeName = "rel")]
+        public string Rel { get; set; }
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+    }
+
+    [XmlRoot(ElementName = "div", Namespace = "http://www.w3.org/1999/xhtml")]
+    public class Div
+    {
+        [XmlAttribute(AttributeName = "class")]
+        public string Class { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+        [XmlElement(ElementName = "h2", Namespace = "http://www.w3.org/1999/xhtml")]
+        public string H2 { get; set; }
+        [XmlElement(ElementName = "div", Namespace = "http://www.w3.org/1999/xhtml")]
+        public List<Div> Divs { get; set; }
+    }
+
+    [XmlRoot(ElementName = "body", Namespace = "http://www.w3.org/1999/xhtml")]
+    public class Body
+    {
+        [XmlElement(ElementName = "div", Namespace = "http://www.w3.org/1999/xhtml")]
+        public Div Wrapper { get; set; }
+    }
+
+    [XmlRoot(ElementName = "html", Namespace = "http://www.w3.org/1999/xhtml")]
+    public class Html
+    {
+        [XmlElement(ElementName = "body", Namespace = "http://www.w3.org/1999/xhtml")]
+        public Body Body { get; set; }
+    }
+}
