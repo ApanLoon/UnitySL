f7e40e3dabac00fc7d53dc5c18afc042a8f4902c
Thor Brigsted <thorbrigsted@gmail.com>
2021-02-17 16:56:51 +0100

    More changes to the chat prefab

---
+++ src/UnitySL/Assets/Scripts/MonoBehaviours/UI/Floater/UIChat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class UIChat : MonoBehaviour
+{
+    public UIMessageLog messageView;
+    public UIChatTabTemplate tabs;
+
+    private void Start()
+    {
+        tabs.Initialize();
+    }
+
+    protected void LogMessage(Logger.LogLevel logLevel, string message)
+    {
+        //message = $"{LogLevelToColour[logLevel]}{DateTime.Now:T} {logLevel} {message}</color>";
+        messageView.AppendMessage(message);
+
+    }
+
+    public class Message
+    {
+        public string sender;
+        public string text;
+    }
+
+    [Serializable] public class UIChatTabTemplate : Template<UIChatTab> { }
+}
