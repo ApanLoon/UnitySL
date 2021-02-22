917649312a4851de699b7e7981d3b802fb8b7d73
Thor Brigsted <thorbrigsted@gmail.com>
2021-02-22 15:29:43 +0100

    Changes to UIChat and UISearchMini

--- src/UnitySL/Assets/Scripts/MonoBehaviours/UI/Floater/UIChat.cs
+++ src/UnitySL/Assets/Scripts/MonoBehaviours/UI/Floater/UIChat.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -9,17 +10,43 @@ public class UIChat : MonoBehaviour
 {
     public UIMessageLog messageView;
     public UIChatTabTemplate tabs;
+    public TMP_InputField chatInputField;
+
+    [NonSerialized] public UIChatTab activeTab;
 
     private void Start()
     {
         tabs.Initialize();
+        // Create placeholder tabs
+        CreateTab("Local", false).toggle.isOn = true;
+        CreateTab("Quackman", true);
+        CreateTab("Bot-6542", true);
+        CreateTab("Skeleton society", true);
     }
 
-    protected void LogMessage(Logger.LogLevel logLevel, string message)
+    /// <summary> Create new chat tab </summary>
+    public UIChatTab CreateTab(string name, bool canClose)
     {
-        //message = $"{LogLevelToColour[logLevel]}{DateTime.Now:T} {logLevel} {message}</color>";
-        messageView.AppendMessage(message);
+        UIChatTab tab = tabs.InstantiateTemplate();
+        tab.name = $"Tab ({name})";
+        tab.label.text = name;
+        tab.canClose = canClose;
+        tab.toggle.onValueChanged.AddListener(isOn => { if (isOn) SelectTab(tab); });
+        return tab;
+    }
 
+    public void SelectTab(UIChatTab tab)
+    {
+        activeTab = tab;
+        messageView.Load(activeTab.messageLog);
+        Debug.Log("Set active tab: " + activeTab.name);
+    }
+
+    public void SendMessage()
+    {
+        activeTab.messageLog.Add(chatInputField.text);
+        messageView.AppendMessage(chatInputField.text);
+        chatInputField.text = "";
     }
 
     public class Message
--- src/UnitySL/Assets/Scripts/MonoBehaviours/UI/Floater/UIChatTab.cs
+++ src/UnitySL/Assets/Scripts/MonoBehaviours/UI/Floater/UIChatTab.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Toggle))]
 public class UIChatTab : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Toggle toggle { get { return _toggle != null ? _toggle : _toggle = GetComponent<Toggle>(); } }
+    private Toggle _toggle;
+    public Text label;
+    [NonSerialized] public readonly List<string> messageLog = new List<string>();
+    [NonSerialized] public bool canClose;
 }
